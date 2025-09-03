e14ced427b8e27dbc9d3aa71c03a14ed00f34e4b
Satyam Singh <satyamrajput7239@gmail.com>
2025-09-03 08:23:53 +0530

    feat : connect to mongodb

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("weer");
+});
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Mongodb Connected");
+  app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+  });
+});
