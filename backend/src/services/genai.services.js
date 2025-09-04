30f35d1dbecdd6c3d2a2e1cf997f3295fdb94f05
Satyam Singh <satyamrajput7239@gmail.com>
2025-09-04 08:40:55 +0530

    feat : add ai to summarize note

--- backend/src/services/genai.services.js
+++ backend/src/services/genai.services.js
@@ -1,8 +1,28 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import OpenAI from "openai";
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.BASE_URL,
+});
+
 export const summarizeText = async (text) => {
-  // Dummy function for summarizing text
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("This is a dummy summary.");
-    }, 1000);
+  const prompt = `Summarize the following note in simple terms:\n\n${text}`;
+
+  const response = await openai.chat.completions.create({
+    model: process.env.MODEL_NAME,
+    messages: [
+      {
+        role: "user",
+        content: text,
+      },
+      {
+        role: "system",
+        content: prompt,
+      },
+    ],
   });
+  return response.choices[0].message.content;
 };
